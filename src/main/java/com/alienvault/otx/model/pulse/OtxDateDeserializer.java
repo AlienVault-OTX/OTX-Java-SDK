d854c0b7455297924b317ad2a613feffd98bd681
Cristobal Rosa <cristobalrosa@gmail.com>
2017-07-12 17:56:12 -0500

    Remove header

--- src/main/java/com/alienvault/otx/model/pulse/OtxDateDeserializer.java
+++ src/main/java/com/alienvault/otx/model/pulse/OtxDateDeserializer.java
@@ -9,18 +9,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/*
- * © AlienVault Inc. 2017
- * All rights reserved
- * This code is protected by copyright and distributed under licenses
- * restricting its use, copying, distribution, and decompilation. It may
- * only be reproduced or used under license from AlienVault Inc. or its
- * authorised licensees.
- *
- * Created by crosa on 12/07/2017.
-*/
 public class OtxDateDeserializer extends JsonDeserializer<Date> {
-
 
     @Override
     public Date deserialize(JsonParser jsonparser,
